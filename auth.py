2442432147f26037f91617189d5d476e4cf39a9c
owenratana <57982080+owenratana@users.noreply.github.com>
2021-02-21 20:40:31 +0700

    add feature to add car to user account

--- auth.py
+++ auth.py
@@ -29,7 +29,7 @@ def login():
     checkValue = cur.execute('SELECT email,password FROM Account WHERE email = %s AND password = %s', (email,password,))
     if checkValue != 0:
         userAcc = cur.fetchone()
-        expiredate = datetime.datetime.utcnow() + datetime.timedelta(seconds=20)
+        expiredate = datetime.datetime.utcnow() + datetime.timedelta(seconds=120)
         token = jwt.encode({'email': userAcc['email'], 'exp' : expiredate}, app.config['SECRET_KEY'])
         mysql.connection.commit()
         cur.close()
--- user.py
+++ user.py
@@ -1,2 +1,24 @@
 from app import *
 
+def addcar():
+    email = request.json['email']
+    platenum = request.json['platenum']
+    city = request.json['city']
+    cur = mysql.connection.cursor()
+    checkValue = cur.execute('SELECT email FROM Cars WHERE platenum = %s AND city = %s', (platenum,city,))
+    if checkValue > 0:
+        carDetail = cur.fetchone()
+        if carDetail['email'] == None:
+            cur.execute('UPDATE Cars SET email = %s WHERE platenum = %s AND city = %s', (email,platenum,city,))
+            mysql.connection.commit()
+            cur.close()
+            return jsonify({'message':'Done'})
+        else:
+            mysql.connection.commit()
+            cur.close()
+            return jsonify({'message':'Car Already Owned'})
+    else:
+        cur.execute('INSERT INTO Cars(platenum,city,email) VALUES(%s,%s,%s)', (platenum,city,email,))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'message':'Done'})
