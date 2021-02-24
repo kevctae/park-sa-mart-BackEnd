ef0e32ff15364f9708bb1527fe04bb7e0d9baa84
Kevin Chakornsiri <38150856+kevctae@users.noreply.github.com>
2021-02-24 16:35:40 +0700

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,4 +1,4 @@
-container=$(docker ps | grep -w test | awk '{print $1}')
+container=$(docker ps | grep -w parksamart | awk '{print $1}')
 if [[ $container ]]
     then
         docker container stop $container
@@ -7,4 +7,4 @@ fi
 
 docker image rm api-parksamart
 docker build -t parksamart:latest .
-docker run -d -p 81:5000 test:latest
+docker run -d -p 81:5000 parksamart:latest
