d264281fac7b0cb848cec83b00d227c4b1147d00
Kevin Chakornsiri <38150856+kevctae@users.noreply.github.com>
2021-02-24 15:49:55 +0700

    Create deploy_script.sh

---
+++ deploy_script.sh
@@ -0,0 +1,10 @@
+container=$(docker ps | grep -w test | awk '{print $1}')
+if [[ $container ]]
+    then
+        docker container stop $container
+        docker container rm $container
+fi
+
+docker image rm api-parksamart
+docker build -t parksamart:latest .
+docker run -d -p 85:5000 test:latest
