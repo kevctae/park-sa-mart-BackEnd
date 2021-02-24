2383d95952d04244197734867c718017f7973bf3
Armi4d <arm.agt26@gmail.com>
2021-02-24 14:18:24 +0700

    Update parkman.py

--- parkman.py
+++ parkman.py
@@ -5,10 +5,10 @@ def visitorentry():
     entrydatetime=request.json['entrydatetime']
     cur = mysql.connection.cursor()
     checkValue = cur.execute ('SELECT plate FROM cars WHERE plate=%s', (plate,))
+    for i in checkValue:
+        string=checkValue[i]
     if checkValue == 0:
         return "Plate not found"
     else:
-        
-
-
-
+    detail = cur.execute ('SELECT entrytime,floor,image FROM parking_record WHERE entrytime=%s AND floor=%s AND image=%s', (entrytime,floor,image,))
+    return jsonify?????
